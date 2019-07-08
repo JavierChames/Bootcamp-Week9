7a40c47e8c1ea9946fcf7dafe3844aa7be29489e
Javier Chames <haimch@gmail.com>
2019-07-08 10:23:27 +0300

    submit

---
+++ Python/Star1/1.py
@@ -0,0 +1,19 @@
+for i in range(1,41):
+     print(i)
+
+a=1
+while a <= 40:
+    print (a)
+    a=a+1
+
+
+for i in range(1,101):
+    if i %3 == 0:
+        print("Fizz " +str(i))
+    if i %5 == 0:
+        print("Buzz "+str(i))
+    if i %5 ==0 and i %3 == 0 :
+         print("FizzBuzz " +str(i) )
+
+
+             
