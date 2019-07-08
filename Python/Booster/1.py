0bdb66afe57a87e81a6222adbed1999dc446012f
Javier Chames <haimch@gmail.com>
2019-07-08 22:24:10 +0300

    submit

---
+++ Python/Booster/1.py
@@ -0,0 +1,5 @@
+my_name="Hello, my name is Inigo Montoya"
+print(my_name[:5])
+print(my_name[7:14])
+print(my_name[0]+ my_name[2:5:2]+ my_name[6]+ my_name[8] + my_name[10:13:2]+my_name[17]+ my_name[16::2])
+print(my_name[3:5]+ my_name[6]+ my_name[8:14:2]+my_name[14:19:2])
