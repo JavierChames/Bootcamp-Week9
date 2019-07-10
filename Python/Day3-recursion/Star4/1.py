00c727e9e5d2dca23de2ec2db67b9ed5682d07c7
Javier Chames <haimch@gmail.com>
2019-07-10 15:25:44 +0300

    submit

---
+++ Python/Day3-recursion/Star4/1.py
@@ -0,0 +1,9 @@
+def palindrome(text):
+  M = len(text)
+  if M <= 1:
+    return True
+  return (text[0] == text[M - 1]) and palindrome(text[1: M - 1])
+  
+
+mystr = "asf"
+print(palindrome(mystr))
